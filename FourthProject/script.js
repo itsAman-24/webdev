994bd1213ee8743d41c39e30f2c4487765a6a24c
itsAman-24 <amanpan2410@gmail.com>
2024-03-29 15:49:18 +0530

    Added

---
+++ FourthProject/script.js
@@ -0,0 +1,9 @@
+let rock = document.querySelector("#rock");
+console.log(rock);
+
+let paper = document.querySelector("#paper");
+console.log(paper);
+
+let scissor = document.querySelector("#scissor");
+console.log(scissor);
+
