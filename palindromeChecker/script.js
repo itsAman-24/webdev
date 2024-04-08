2fa5dc39b101309b2aba39500f3ec10e1d72e16c
itsAman-24 <amanpan2410@gmail.com>
2024-04-08 07:49:21 +0530

    Added

--- palindromeChecker/script.js
+++ palindromeChecker/script.js
@@ -4,12 +4,13 @@ let submitBtn = document.querySelector("#submit");
 
 function reverseString(str) {
     return str.split('').reverse().join('');
+
 }
 
 submitBtn.addEventListener("click" , () => {
     const inputedText = inputString.value;
     const reversedText = reverseString(inputedText);
-
+    
     if(inputedText === reversedText) {
         // document.body.innerHTML = "<h1> It is a palindrome </h1> "
         alert("It is a Palindrome");
